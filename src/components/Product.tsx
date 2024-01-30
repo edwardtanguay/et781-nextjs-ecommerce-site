37195354b85c0dfdc7be42772e7acda1e7e03077
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-30 14:40:20 +0100

    add to cart button in

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -9,16 +9,21 @@ export const Product = ({ product, quantity }: IProps) => {
 	return (
 		<div
 			key={product.id}
-			className="mb-4 bg-slate-600 p-4 rounded w-1/2 flex gap-3"
+			className="mb-4 bg-slate-600 p-4 rounded w-1/2 flex gap-3 max-w-[35rem]"
 		>
 			{quantity && <p className="text-slate-100 text-2xl">{quantity}x</p>}
 			<img src={`images/products/product_${product.id}.jpg`} />
-			<section>
-				<p className="text-xs text-yellow-500">{product.category}</p>
-				<p className="font-semibold text-2xl">
-					{product.brand} {product.model}
-				</p>
-				<p className="text-red-500">{product.price} €</p>
+			<section className="productGrid">
+				<section>
+					<p className="text-xs text-yellow-500">
+						{product.category}
+					</p>
+					<p className="font-semibold text-2xl">
+						{product.brand} {product.model}
+					</p>
+					<p className="text-red-500">{product.price} €</p>
+				</section>
+				<button type="button" className="bg-slate-400 rounded">Add to Cart</button>
 			</section>
 		</div>
 	);
