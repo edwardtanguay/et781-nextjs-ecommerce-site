e6ba861ed20e3a2a6f8fb21466e33523d24a1f44
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-30 11:53:52 +0100

    photos in

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 import db from "@/data/db.json";
 
 const products = db.products;
@@ -5,13 +6,15 @@ const products = db.products;
 export default function Products() {
 	return (
 		<main>
-			<h2 className="text-xl mb-3">Products</h2>
 			{products.map((product) => {
 				return (
-					<div key={product.id} className="mb-4 bg-slate-600 p-4 rounded w-1/2">
+					<div key={product.id} className="mb-4 bg-slate-600 p-4 rounded w-1/2 flex gap-3">
+						<img src={`images/products/product_${product.id}.jpg`}/>
+						<section>
 						<p className="text-xs text-yellow-500">{product.category}</p>
 						<p className="font-semibold text-2xl">{product.brand} {product.model}</p>
 						<p className="text-red-500">{product.price} €</p>
+						</section>
 					</div>
 				);
 			})}
