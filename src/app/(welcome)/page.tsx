64ef366961bd3cfc1aa1fb91fd6dd08f187e91ed
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-30 15:07:12 +0100

    product of week in

--- src/app/(welcome)/page.tsx
+++ src/app/(welcome)/page.tsx
@@ -1,13 +1,23 @@
 "use client";
 import { AppContext } from "@/AppContext";
+import { Product } from "@/components/Product";
 import { useContext } from "react";
 
 export default function Home() {
 	const { products } = useContext(AppContext);
+
+	const productOfTheWeek = products.find((m) => m.id === 6);
+
 	return (
 		<main>
 			<p className="mb-3">Welcome to our tech products store.</p>
-			<p className="mb-3">We have {products.length} products for you to choose from.</p>
+			<p className="mb-3">
+				We have {products.length} products for you to choose from.
+			</p>
+			<div className="mb-3">
+				<p className="font-semibold">Product of the week:</p>
+				{productOfTheWeek && <Product product={productOfTheWeek} />}
+			</div>
 		</main>
 	);
 }
