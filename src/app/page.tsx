ab492200976815bd5fb2c43828f3d176ed6a4de2
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-30 11:00:35 +0100

    showing products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
-import Image from "next/image";
+import db from "@/data/db.json";
+
+const products = db.products;
 
 export default function Home() {
-  return (
-    <main>
-      <h1 className="text-2xl mb-3">E-Commerce Site</h1>
-      <p>Welcome.</p>
-    </main>
-  );
+	return (
+		<main>
+			<h1 className="text-2xl mb-3">E-Commerce Site</h1>
+			<p>Welcome.</p>
+			<h2 className="text-2xl mt-6">Products</h2>
+			{products.map((product) => {
+				return (
+					<div key={product.id}>
+						<p>{product.model}</p>
+					</div>
+				);
+			})}
+		</main>
+	);
 }
