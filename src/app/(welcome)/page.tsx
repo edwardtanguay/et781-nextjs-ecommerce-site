85c1fcf3b8983aca3bb03a607230ae35c62a7fd2
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-30 13:35:20 +0100

    products on welcome page

--- src/app/(welcome)/page.tsx
+++ src/app/(welcome)/page.tsx
@@ -1,11 +1,13 @@
-import db from "@/data/db.json";
-
-const products = db.products;
+"use client";
+import { AppContext } from "@/AppContext";
+import { useContext } from "react";
 
 export default function Home() {
+	const { products } = useContext(AppContext);
 	return (
 		<main>
-			<p>Welcome.</p>
+			<p className="mb-3">Welcome to our tech products store.</p>
+			<p className="mb-3">We have {products.length} products for you to choose from.</p>
 		</main>
 	);
 }
